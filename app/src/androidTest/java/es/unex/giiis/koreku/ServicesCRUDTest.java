32333f92ed299aa8b88acbe49a3438ec45c6b4a4
gguiberteau <gguibert@alumnos.unex.es>
2021-12-06 00:03:54 +0100

    ServicesCRUDTest testeado al completo y funcional (con todas las operaciones de la BD)

--- app/src/androidTest/java/es/unex/giiis/koreku/ServicesCRUDTest.java
+++ app/src/androidTest/java/es/unex/giiis/koreku/ServicesCRUDTest.java
@@ -3,6 +3,7 @@ package es.unex.giiis.koreku;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import android.content.Context;
 
@@ -100,6 +101,102 @@ public class ServicesCRUDTest {
 
         // Se inician las comprobaciones de que el update se ha realizado correctamente
         assertEquals(serviceList.get(0).getTitle(), "Prime del JPery");
+
+    }
+
+    @Test
+    public void shouldDeleteAllItemsOnList() throws Exception{
+
+        // Se crean los Items a insertar
+        Service s1 = new Service(1, "Prime de Knekro", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+        Service s2 = new Service(2, "Prime Felipez", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+        Service s3 = new Service(3, "Prime de JPery", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+        Service s4 = new Service(4, "Prime de Ander", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+
+        // Se insertan los items
+        serviceDAO.insert(s1);
+        serviceDAO.insert(s2);
+        serviceDAO.insert(s3);
+        serviceDAO.insert(s4);
+
+        //Se borran TODOS los services
+        serviceDAO.deleteAll();
+
+        // Se recupera en el LiveData
+        LiveData<List<Service>> liveServices = serviceDAO.getAll();
+        List<Service> serviceList = LiveDataTestUtils.getValue(liveServices);
+
+        // Se comprueba que se han borrado correctamente
+        assertEquals(serviceList.size(), 0);
+
+    }
+
+    @Test
+    public void shouldDeleteOneItemOnList() throws Exception{
+
+        // Se crean los Items a insertar
+        Service s1 = new Service(1, "Prime de Knekro", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+        Service s2 = new Service(2, "Prime Felipez", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+        Service s3 = new Service(3, "Prime de JPery", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+        Service s4 = new Service(4, "Prime de Ander", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+
+        // Se insertan los items
+        serviceDAO.insert(s1);
+        serviceDAO.insert(s2);
+        serviceDAO.insert(s3);
+        serviceDAO.insert(s4);
+
+        // Borramos uno de ellos
+        serviceDAO.deleteService("Prime de JPery");
+
+        // Se recupera el LiveData
+        LiveData<List<Service>> liveServices = serviceDAO.getAll();
+        List<Service> serviceList = LiveDataTestUtils.getValue(liveServices);
+
+        // Se comprueba que se ha borrado correctamente
+        assertEquals(serviceList.size(), 3);
+        assertFalse(serviceList.get(0).getTitle().equals("Prime de JPery"));
+        assertFalse(serviceList.get(1).getTitle().equals("Prime de JPery"));
+        assertFalse(serviceList.get(2).getTitle().equals("Prime de JPery"));
+
+    }
+
+    @Test
+    public void shouldReturnAllItemsOrderedByDueDate() throws Exception{
+
+        // Se crean los Items a insertar
+        Service s1 = new Service(1, "Prime de Knekro", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2024, 1, 6));
+        Service s2 = new Service(2, "Prime Felipez", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2023, 1, 6));
+        Service s3 = new Service(3, "Prime de JPery", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2022, 1, 6));
+        Service s4 = new Service(4, "Prime de Ander", "Twitch Prime",
+                "gguibert@alumnos.unex.es", "1", new Date(2021, 1, 6));
+
+        // Se insertan los items
+        serviceDAO.insert(s1);
+        serviceDAO.insert(s2);
+        serviceDAO.insert(s3);
+        serviceDAO.insert(s4);
+
+        // Se recupera en el LiveData
+        LiveData<List<Service>> liveServices = serviceDAO.getAllByDueDate();
+        List<Service> serviceList = LiveDataTestUtils.getValue(liveServices);
+
+        // Se comprueba que se han ordenado correctamente
+        assertEquals(serviceList.get(0).getId(), 4);
+        assertEquals(serviceList.get(1).getId(), 3);
+        assertEquals(serviceList.get(2).getId(), 2);
+        assertEquals(serviceList.get(3).getId(), 1);
 
     }
 
