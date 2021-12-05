231be0791e107b958b710aae085c06eb97054711
alvaronp <nietoparedes.alvaro@gmail.com>
2021-12-05 16:32:28 +0100

    clases de prueba creadas

--- app/src/test/java/es/unex/giiis/koreku/ProfileCRUDTest.java
+++ app/src/test/java/es/unex/giiis/koreku/ProfileCRUDTest.java
@@ -1,15 +1,15 @@
 package es.unex.giiis.koreku;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ProfileCRUDTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
