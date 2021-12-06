4ebc69332badca179a04f24d56e1834529cec7f9
alvaronp <nietoparedes.alvaro@gmail.com>
2021-12-06 11:49:46 +0100

    importado mas espresso

--- app/src/androidTest/java/es/unex/giiis/koreku/UIConsolesTest.java
+++ app/src/androidTest/java/es/unex/giiis/koreku/UIConsolesTest.java
@@ -1,12 +1,29 @@
 package es.unex.giiis.koreku;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openContextualActionModeOverflowMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
+import android.widget.DatePicker;
 
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +33,38 @@ import org.junit.runner.RunWith;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class UIConsolesTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("es.unex.giiis.koreku", appContext.getPackageName());
+    public void shouldAddConsoleToRecyclerView(){
+        onView(withId(R.id.navigation_consoles)).perform(click());
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.title)).perform(typeText("tituloConsola"), closeSoftKeyboard());
+        onView(withId(R.id.desc)).perform(typeText("companyConsola"), closeSoftKeyboard());
+        onView(withId(R.id.date_picker_button)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(1989, 8, 25));
+
+        // Perform a click() action on R.id.submitButton
+        onView(withId(R.id.submitButton)).perform(click());
+
+        // Check that R.id.my_recycler_view hasDescendant withId R.id.titleView
+        onView(withId(R.id.my_recycler_view)).check(matches(hasDescendant(withId(R.id.titleView))));
+        // Check that R.id.my_recycler_view hasDescendant with the input text
+        onView(withId(R.id.my_recycler_view)).check(matches(hasDescendant(withText(testingString))));
+        // Check that R.id.my_recycler_view hasDescendant withId R.id.statusCheckBox
+        onView(withId(R.id.my_recycler_view)).check(matches(hasDescendant(withId(R.id.statusCheckBox))));
+    }
+
+    @After
+    public void deleteElements(){
+        // Open Contextual Action Mode Overflow Menu (abrir menu de 3 puntos)
+        openContextualActionModeOverflowMenu();
+        // Perform a click() action on the view withText "Delete all" (Should be a R.string.* reference)
+        onView(withText(R.string.delete_all_menu_button)).perform(click());
     }
 }
